a1a43ffdddfe062cb23b719d402532efc774e9c9
meltem ertürk <meltemerturk95@gmail.com>
2024-12-20 14:26:35 +0300

    Add project files.

---
+++ patikaDev1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Bu, ekrana yazı yazmak gibi temel işlemleri yapmamızı sağlar.
+using System;
+
+class Program
+{
+    // Buradan itibaren program çalışmaya başlar.
+    // "Main" metodumuz, C# programlarının başlangıç noktasıdır.
+    static void Main(string[] args)
+    {
+        // Ekrana adımızı yazdırıyoruz.
+        Console.WriteLine("Meltem Ertürk");
+
+        // Ekrana heyecanımızı belirten bir mesaj daha yazdırıyoruz.
+        Console.WriteLine("Yazılıma ilgim var ve bu bootcamp'e başlamanın heyecanını yaşıyorum!");
+    }
+}
